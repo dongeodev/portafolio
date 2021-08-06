7432432a63397834711fb0f51b21a32bfb8566a4
DonGeo <geo.du07@gmail.com>
2021-08-06 17:36:40 -0500

    add meta datos

--- src/components/Seo/data.js
+++ src/components/Seo/data.js
@@ -2,9 +2,11 @@ export const seo = {
   title: "donGeo",
   siteTitle: "Home",
   description: "El sitio web de un desarrollador frontend",
+  url: "https://www.dongeo.dev/",
 };
 export const knowMe = {
   title: "donGeo",
   siteTitle: "Know Me",
   description: "Conoce mis habilidades frontend",
+  url: "https://www.dongeo.dev/",
 };
--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function SEO({ description, title, siteTitle }) {
+export default function SEO({ description, title, siteTitle, url }) {
   return (
     <Head>
       <title>{`${title} | ${siteTitle}`}</title>
@@ -9,6 +9,9 @@ export default function SEO({ description, title, siteTitle }) {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content="https://i.ibb.co/sJj6k3d/dongeo.png" />
       <link rel="shortcut icon" type="image/x-icon" href="/dev.ico"></link>
     </Head>
   );
